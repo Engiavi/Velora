9a1439eae9fa42d3efc06c1d71512abb27e1c308
Engiavi <hackavi796@gmail.com>
2025-01-11 19:46:44 +0530

    initialising file structures and writing backend code and installing packages

---
+++ scripts/loadDb.ts
@@ -0,0 +1,11 @@
+import {DataAPIClient} from "@datastax/astra-db-ts"
+import {puppeteerWebBaseLoader} from "langchain/document_loaders/web/puppeteer"
+
+// import OpenAI from "openai"
+
+import "dotenv/config"
+
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter"
+
+
+ 
