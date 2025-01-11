ba741028313ce5657c6f796adbbd37754bfc760c
Engiavi <hackavi796@gmail.com>
2025-01-11 20:00:48 +0530

    creating a connection to database

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -1,11 +1,33 @@
-import {DataAPIClient} from "@datastax/astra-db-ts"
-import {puppeteerWebBaseLoader} from "langchain/document_loaders/web/puppeteer"
+import { DataAPIClient } from "@datastax/astra-db-ts"
+import { puppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer"
 
-// import OpenAI from "openai"
+import OpenAI from "openai"
 
 import "dotenv/config"
 
-import {RecursiveCharacterTextSplitter} from "langchain/text_splitter"
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 
+const {
+    DB_NAMESPACE,
+    DB_COLLECTION,
+    DB_API_ENDPOINT,
+    DB_APPLICATION_TOKEN,
+    HUGGINGFACE_API
+} = process.env
 
- 
+const openai = new OpenAI({apiKey: HUGGINGFACE_API})
+
+const veloraData =[
+    "https://segment.com/docs/?ref=nav",
+    "https://docs.mparticle.com/",
+    "https://docs.lytics.com/",
+    "https://docs.zeotap.com/home/en-us/"
+]
+
+const client = new DataAPIClient(DB_APPLICATION_TOKEN)
+const db = client.db(DB_API_ENDPOINT,{namespace: DB_NAMESPACE})
+
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize:512,
+    chunkOverlap: 100
+})
