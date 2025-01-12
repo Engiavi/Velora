ff17e19e3c8c2ed2be247fd3b807e65e6ad3a569
Engiavi <hackavi796@gmail.com>
2025-01-12 12:11:44 +0530

    added css file and also add chat content code

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,40 @@
 "use client"
-
+import "./global.css"
 import Image from 'next/image'
 
 import logo from './assets/logo.png'
 
-import {useChat} from "ai/react"
+import { useChat } from "ai/react"
 
-import{Message} from "ai"
+import { Message } from "ai"
 
-const Home =()=>{
-    const {append,isLoading,messages,input, handleInputChange, handleSubmit} = useChat()
-    const noMessage = false
+const Home = () => {
+    const { append, isLoading, messages, input, handleInputChange, handleSubmit } = useChat()
+    const noMessage = true
     return (
         <main>
-            <Image src={logo} alt="logo" width={150} height={50}/>
-            <section>
-                {noMessage ?(
+            <Image src={logo} alt="logo" width={150} height={50} />
+            <section className={noMessage ? "" : "populated"}>
+                {noMessage ? (
                     <>
-                    <p className='starter-text'>
-                        The Ultimate place for 
-                    </p>
-                    <br/>
-                    {/* <PromptSuggestionRow/> */}
+                        <p className='starter-text'>
+                            The Ultimate place for
+                        </p>
+                        <br />
+                        {/* <PromptSuggestionRow/> */}
                     </>
-                ):(
+                ) : (
                     <>
-                    {/* map message */}
-                    {/* <LoadingBubble/> */}
+                        {/* map message */}
+                        {/* <LoadingBubble/> */}
                     </>
                 )}
-                <form onSubmit={handleSubmit}>
-                    <input className='question-box' onChange={handleInputChange} value={input} placeholder='Ask me something...'/>
-                    <input type="submit" />
-                </form>
+
             </section>
+            <form onSubmit={handleSubmit}>
+                <input className='question-box' onChange={handleInputChange} value={input} placeholder='Ask me something...' />
+                <input type="submit" />
+            </form>
         </main>
     )
 }
